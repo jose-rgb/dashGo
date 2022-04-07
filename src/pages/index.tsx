0985064404c7931c4faf35756d196db0564a9e34
jose-rgb <josericardosaraiva7@gmail.com>
2022-04-07 15:45:26 -0300

    criada page Sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import { Button, Flex, Input } from "@chakra-ui/react"
 
 export default function Home() {
   return (
-    <>
-      <h1>ola</h1>
-    </>
+    <Flex w="100vw" h="100vh" align="center" justify="center" >
+      
+      <Flex as="form" w="100%" maxWidth="360px" bg="gray.800" p="8" borderRadius="8px" flexDir="column" >
+       
+        <Input name="email"  type="email" placeholder="Email" focusBorderColor="pink.500" bgColor="gray.900" variant="filled" size="lg" _hover={{bgColor: 'gray.900'}} mb="6"/>
+        <Input name="password"  type="password" placeholder="Senha" focusBorderColor="pink.500" bgColor="gray.900" variant="filled" size="lg" _hover={{bgColor: 'gray.900'}}/>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg" >Entrar</Button>
+
+      </Flex>
+
+    </Flex>
   )
 }

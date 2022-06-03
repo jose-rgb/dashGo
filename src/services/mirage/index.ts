fdda1ecb13037187bffbfefa6e13f9b39d702239
jose-rgb <josericardosaraiva7@gmail.com>
2022-06-03 08:32:13 -0300

    logica de pagination com miraje

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from "miragejs";
+import { createServer, Factory, Model, Response } from "miragejs";
 import faker from 'faker';
 
 type User = {
@@ -33,7 +33,7 @@ export function makeServer() {
 
         seeds(server) {
             //nome do factorie, e quantidade a ser criada
-            server.createList('user',10)
+            server.createList('user',200)
         },
 
         //quais rotas o server vai ter
@@ -41,7 +41,26 @@ export function makeServer() {
             this.namespace = 'api';
             this.timing = 750;
 
-            this.get('/users')
+            this.get('/users', function (schema, request) {
+                //po padrao mostrar page 1, 10 registros por page
+                const { page = 1 , per_page = 10} =  request.queryParams
+
+                //quantos registros existem no model users
+                const total = schema.all('user').length
+
+                //page 2, registros do 10 a 20
+                const  pageStart = (Number(page) - 1) * Number(per_page);  // page = 3, 3 -1 = 2, 2 * 10 = 20
+                const pageEnd = pageStart + Number(per_page)                // 20 + 10 = 30, do registro 20 á 30
+
+                const users = this.serialize(schema.all('user')).users.slice(pageStart, pageEnd)
+
+                return new Response(
+                    200,
+                    {'x-total-count': String(total)},
+                    { users }
+                )
+            })
+
             this.post('/users')
 
             this.namespace = '';

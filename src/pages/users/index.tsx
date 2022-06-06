8b6363aa6ce81cfaaf5b4fd245fe7b7c3fcb7068
jose-rgb <josericardosaraiva7@gmail.com>
2022-06-06 09:41:32 -0300

    add component pagination funcional

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -106,7 +106,7 @@ export default function UserList(){
 
                             </Table>
 
-                            <Pagination />
+                            <Pagination  totalCountOfRegisters={200} currentPage={5} onPageChange={()=>{}} />
                         </>
                     )}
 
